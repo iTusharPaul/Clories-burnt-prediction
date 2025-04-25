3dd6e4f76061b376fec236c1a5844066f08028d5
iTusharPaul <tushar.study143@gmail.com>
2025-04-25 21:05:10 +0530

    Update CaloriesBurntPrediction.py
    removed duration and multiplied with duration for final prediction

--- CaloriesBurntPrediction.py
+++ CaloriesBurntPrediction.py
@@ -13,7 +13,7 @@ import pickle
 #loading the saved model
 loaded_model = pickle.load(open('C:/Users/KIIT/Desktop/Machine Learning Specialization/projects/Clories burnt prediction/loaded_model.sav','rb'))
 
-input_data = (0,68,190.0,94.0,29.0,105.0,40.8)
+input_data = (0,68,190.0,94.0,105.0,40.8)
 input_data_as_array = np.asarray(input_data)
 
 input_data_reshaped = input_data_as_array.reshape(1,-1)
@@ -23,4 +23,4 @@ print(input_data_reshaped)
 
 
 prediction = loaded_model.predict(input_data_reshaped) # we are using loaded model from the file for prediction
-print(f"You have Successfully burnt {prediction} calories!!!")
+print(f"You have Successfully burnt {prediction*29.0} calories!!!")
